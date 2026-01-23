72136ddb4c0b67448b0006cf18d84ecbf0199956
Leo262222 <139959823+Leo262222@users.noreply.github.com>
2026-01-23 10:17:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  }
+  build: {
+    rollupOptions: {
+      input: {
+        // 这里告诉 Vercel：我有两个家！
+        main: 'index.html',  // 主页
+        admin: 'admin.html', // 后台管理页
+      },
+    },
+  },
 })
